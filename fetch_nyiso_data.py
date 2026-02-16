ac811009b151b519eb1e622958cd59d1074928d4
CodeGoat-tech <jaygetmunyun8@gmail.com>
2026-02-16 15:14:45 -0500

    Delete fetch_nyiso_data.py

--- fetch_nyiso_data.py
+++
@@ -1,5 +0,0 @@
-from nyisotoolkit import NYISOData
-
-df_prices = NYISOData(dataset="lbmp_rt_5m", year="2023").df
-df_dam = NYISOData(dataset="lbmp_dam_h", year="2023").df
-df_load = NYISOData(dataset="load_5m", year="2023").df
